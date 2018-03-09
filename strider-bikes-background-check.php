a7262b3b7503a80e7fa4f8039db2e0db470bbad2
Nick Williams <nick.williams@strider.local>
2018-03-09 15:30:43 -0700

    first commit, adds checkbox to user profile (admin)

---
+++ strider-bikes-background-check.php
@@ -0,0 +1,106 @@
+<?php 
+/*
+Plugin Name: Strider Bikes User Background Check 
+Plugin URI: https://github.com/nickwilliamsnewby
+Description: adds meta boxes to all pages/posts to restrict access until an lpUser completes the selected course
+Author: Nicholas Williams
+Version: 1.0.0
+Author URI: http://williamssoftwaresolutions.com
+Tags: learnpress
+Text Domain: sbbgCheck
+*/
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+if(! defined( 'STRIDER_BIKES_BGCHECK_PATH' ) ) define('STRIDER_BIKES_BGCHECK_PATH', dirname( __FILE__ ) );
+if(! defined( 'STRIDER_BIKES_BGCHECK_FILE' ) ) define('STRIDER_BIKES_BGCHECK_FILE', ( __FILE__ ) );
+
+
+if ( !defined('ABSPATH')) {
+    exit;
+}
+
+
+class Strider_Bikes_Background_Check{
+
+	/**
+	 * @var object
+	 */
+	private static $_instance = false;
+
+	/**
+	 * @var string
+	 */
+	private $_plugin_url = '';
+
+	/**
+	 * @var string
+	 */
+    private $_plugin_template_path = '';
+
+    //protected $_meta_boxes = array();
+
+    //protected $_post_type = '';
+
+
+
+
+    function __construct(){
+        //$this->_post_type = 'lp_unlock_oncomplete_cpt';
+        $this->_tab_slug = sanitize_title( 'sb-background-check' );
+        $this->_plugin_template_path = STRIDER_BIKES_BGCHECK_PATH.'/template/';
+        $this->_plugin_url  = untrailingslashit( plugins_url( '/', STRIDER_BIKES_BGCHECK_FILE ));
+        /*
+        add_action('manage_users_columns', array($this, 'strider_bikes_add_user_bg_metaBool'));
+        add_filter('manage_users_custom_column', array($this, 'sb_modify_background_check_row'), 10, 3);
+        */
+        add_action('show_user_profile', array($this, 'sb_bg_bool_profile'));
+        add_action('edit_user_profile', array($this, 'sb_bg_bool_profile'));
+    }
+
+    function sb_bg_bool_profile($profileuser) {
+        ?>
+            <table class="form-table">
+                <tr>
+                    <th>
+                        <label for="user_location"><?php _e('Back Ground Check Status'); ?></label>
+                    </th>
+                    <td>
+                        <input type="checkbox" name="user_bg_check_passed" id="user_bg_check_passed" value="<?php echo esc_attr( get_the_author_meta( 'user_bg_check_passed', $profileuser->ID ) ); ?>" />
+                        <br><span class="description"><?php _e('Background Check Status', 'text-domain'); ?></span>
+                    </td>
+                </tr>
+            </table>
+        <?php
+        }
+    function sb_modify_background_check_row($value, $column_name, $user_id){
+        switch($column_name){
+            case 'bgCheckPassed':
+                return get_the_author_meta('bgCheckPassed', $user_id);
+                break;
+            default:
+                break;
+        }
+        return $value;
+    }
+
+    function strider_bikes_add_user_bg_metaBool( $column ) {
+        $column['bgCheckPassed'] = 'Background check status';
+        return $column;
+    }
+
+	/**
+	 * @return bool|Strider_Bikes_Background_Check because OOP is fun
+	 */
+	static function instance() {
+		if ( !self::$_instance ) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
+
+}
+//create an instance of our add - ons main class 
+add_action( 'init', array( 'Strider_Bikes_Background_Check', 'instance' ) );
