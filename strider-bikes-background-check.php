8f16290a769eb9e32fb9a8b67fa83a2e419cba7b
Nick Williams <nick.williams@strider.local>
2018-03-21 16:26:24 -0600

    added checkbox to restrict pages from users who have not passed the background check

--- strider-bikes-background-check.php
+++ strider-bikes-background-check.php
@@ -53,10 +53,9 @@ class Strider_Bikes_Background_Check{
         $this->_grav_id = get_option('sb_bg_check_abg_grav_ID');
         $this->_plugin_template_path = STRIDER_BIKES_BGCHECK_PATH.'/templates/';
         $this->_plugin_url  = untrailingslashit( plugins_url( '/', STRIDER_BIKES_BGCHECK_FILE ));
-        /*
-        add_action('manage_users_columns', array($this, 'strider_bikes_add_user_bg_metaBool'));
-        add_filter('manage_users_custom_column', array($this, 'sb_modify_background_check_row'), 10, 3);
-        */
+
+        add_action( 'load-post.php', array( $this, 'sb_bg_add_meta_boxes' ), 0 );
+        add_action( 'load-post-new.php', array( $this, 'sb_bg_add_meta_boxes' ), 0 );
         add_action('admin_menu', array($this, 'sb_bg_check_create_menu'));
         add_action('gform_pre_submission_'.$this->_grav_id, array($this, 'sb_bg_pre_gravity_form'));
         add_action('gform_after_submission_5', array($this, 'sb_bg_check_make_order_grav_forms'));
@@ -71,8 +70,94 @@ class Strider_Bikes_Background_Check{
         add_action('wp_ajax_check_make_order', array($this, 'sb_bg_check_make_order'));
         add_action('wp_ajax_check_update_userInfo', array($this, 'sb_bg_check_update_userInfo'));
         add_action('wp_ajax_check_order_status', array($this, 'sb_bg_check_order_status'));
+        add_action('wp', array($this, 'restrict_until_complete_maybe'));
+        add_action('wp', array($this, 'add_menu_filter'));
     }
-    
+
+    function add_menu_filter(){
+        add_filter('nav_menu_link_attributes', array($this,'sb_bg_hide_appropriate_nav_links'), 10, 3);
+    }
+    function sb_bg_hide_appropriate_nav_links($atts, $item, $args){
+        if( $args->menu == 'primary' ){
+            $id = $item->object_id;
+            $itemUnlocked = $this->lp_unlock_check_ze_page($id, 'sb_bg_lock_until_passed_check');
+            if (!$itemUnlocked){
+                $atts['style'] = 'display: none;';
+            }
+            $itemUnlocked = $this->lp_unlock_check_ze_page($id, 'sb_bg_lock_until_logged_in');
+            if (!$itemUnlocked){
+                $atts['style'] = 'display: none;';
+            }
+        }
+        return $atts;
+    }
+
+   // add meta box to set page as locked until background check is complete
+    public function sb_bg_add_meta_boxes() {
+        $prefix                                        = '_lp_';
+        new RW_Meta_Box(
+            apply_filters( 'sb_bg_lock_page_until_bgCheck', array(
+                    'title'      => 'BackGround Check Page Lock',
+                    'post_types' => 'page',
+                    'context'    => 'normal',
+                    'priority'   => 'high',
+                    'fields'     => array(
+                        array(
+                            'name'        => 'Locked (checked = locked, unchecked = not locked)',
+                            'id'          => "sb_bg_lock_until_passed_check",
+                            'type'        => 'checkbox',
+                            'description' => __('Do you want to block this page from user who havent passed a Background Check', 'sbbgCheck'),
+                            'std'         => 0
+                        ),
+                        array(
+                            'name'        => 'Logged in Users Only',
+                            'id'          => "sb_bg_lock_until_logged_in",
+                            'type'        => 'checkbox',
+                            'description' => __('Do you want to block this page from user who are not Logged In', 'sbbgCheck'),
+                            'std'         => 0
+                        )
+                    )
+                )
+            )
+        );
+        }
+
+    function restrict_until_complete_maybe(){
+            global $wp_query;
+            $pID = $wp_query->get_queried_object_id();
+            $unlocked = $this->lp_unlock_check_ze_page($pID, 'sb_bg_lock_until_logged_in');
+            if (!$unlocked){
+                wp_redirect(get_site_url());
+                exit;
+            }
+            $unlocked = $this->lp_unlock_check_ze_page($pID, 'sb_bg_lock_until_passed_check');
+            if (!$unlocked){
+                wp_redirect(get_site_url());
+                exit;
+            }
+        }
+
+    function lp_unlock_check_ze_page($cPageId, $metaKey){
+            $cUser = learn_press_get_current_user();
+            $lockVar = get_post_meta($cPageId, $metaKey, false)[0];
+            $isUnlocked = true;
+            if($lockVar<1){
+                return $isUnlocked;
+            } else {
+                if($metaKey == 'sb_bg_lock_until_logged_in' && !$cUser->ID){
+                    return false;
+                }
+                if ($metaKey == 'sb_bg_lock_until_passed_check'){
+                    $uID = $cUser->ID;
+                    $bgStatus = get_user_meta($uID, 'user_bg_check_passed', false)[0];
+                    if ($bgStatus == 0){
+                        $isUnlocked = false;
+                    }
+                }
+            }
+            return $isUnlocked;
+        }
+
     function sb_bg_check_create_menu() {
     
         //create new top-level menu
